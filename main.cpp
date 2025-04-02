82673f01993becb2fcbfb4b4ff399d8c7c93b269
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-04-02 11:16:05 +0500

    vv

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "MyStack.h"
+using namespace std;
+int main() {
+    MyStack<int> stack;
+
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    cout << "Top element: " << stack.top() << endl; // 30
+    stack.pop();
+    cout << "Top element after pop: " << stack.top() << endl; // 20
+    stack.pop();
+    stack.pop();
+    // This should throw an exception
+    try {
+        stack.pop();
+    } catch (const std::logic_error &e) {
+        cout << "Exception caught: " << e.what() << endl;
+    }
+    return 0;
+}
